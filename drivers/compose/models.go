7f76150aeff9cff33845832c21720ef7a68f37a1
Jitendra Takalkar <jitendra.takalkar@atos.net>
2016-06-27 22:14:30 -0700

    Renamed 'brooklyn' to compose

---
+++ drivers/compose/models.go
@@ -0,0 +1,26 @@
+package compose
+
+type Application struct {
+	Name         string
+	Location     string
+	Type         string
+	SshUserKey   string
+	OsName       string
+	OsVersion    string
+	TemplateSize string
+}
+
+func NewApplication() *Application {
+	return &Application{}
+}
+
+type HostAndPort struct {
+	Host                 string
+	Port                 int
+	HasBracketlessColons bool
+}
+
+type SshHostAddress struct {
+	User        string
+	HostAndPort HostAndPort
+}
